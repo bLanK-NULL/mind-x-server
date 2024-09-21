94b6a82d6391a1f20468a1b2120613c2082aaf09
bLanK-NULL <2522385400@qq.com>
2024-09-21 18:26:22 +0800

    [feat] support docker

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,11 +1,15 @@
 const config = {
     // 服务器数据库
     dataBase: {
-        DATABASE: 'mind-x',
+        DATABASE: 'mind-x-docker',
         USERNAME: 'root',
         PASSWORD: '252238Lzy',
-        PORT: '3306',
-        HOST: 'localhost',
+        PORT: 3306,
+        // HOST: 'localhost',
+        // PORT: 3307,
+        // HOST: 'mysql',
+        HOST: 'mind-x-db',
+        // HOST: '1.94.9.34',
         multipleStatements: true //允许一次执行多条sql
     }
 }
